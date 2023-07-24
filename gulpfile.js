d1b7b57b7f8fe794d66fb8015503778909823b65
Nikita Khovdun <n.kvovdun@samolet.ru>
2023-07-24 14:00:21 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { watch, src, dest, parallel } = require('gulp')
+const rename = require('gulp-rename')
+const bundle = require('gulp-esbuild')
+const connect = require('gulp-connect')
+
+const copyWorkspaceTemplate = async () => {
+  return src('./src/workspace/index.html')
+    .pipe(rename({ dirname: '' }))
+    .pipe(dest('/.serve'))
+}
+
+const buildWorkspaceScript = async () => {
+  return src('./src/workspace/index.ts')
+    .pipe(bundle({
+        outfile: 'bundle.js',
+        bundle: true,
+        loader: {
+          '.mustache': 'text',
+        },
+        treeShaking: true,
+        sourcemap: true,
+        target: "es6",
+        charset: 'utf8',
+    }))
+    .pipe(dest('./dist'))
+}
+
+const buildLib = async () => {
+  return src('./src/lib/index.ts')
+    .pipe(bundle({
+        outfile: 'bundle.js',
+        bundle: true,
+        treeShaking: true,
+        sourcemap: true,
+        target: "es6",
+        charset: 'utf8',
+    }))
+    .pipe(dest('./dist'))
+}
+
+exports.default = buildLib
+
+const serve = async () => {
+  await buildWorkspaceScript()
+  await copyWorkspaceTemplate()
+  connect.server({
+    root: './.serve',
+    livereload: true,
+    port: 8080,
+  })
+}
+
+const listen = async () => {
+  watch(['./src/**/*.ts', './src/lib/**/*.mustache']).on('change', async (path) => {
+    await buildWorkspaceScript()
+    return src(path, { read: false }).pipe(connect.reload())
+  })
+}
+
+exports.dev = parallel(serve, listen)
